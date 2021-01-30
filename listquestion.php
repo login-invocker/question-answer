b49352314147e715ac1296d7beae99d1a68dee86
vuong <vuong.x510@gmail.com>
2021-01-30 11:09:32 +0700

    add api and update ui list question

--- listquestion.php
+++ listquestion.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Danh sách câu hỏi</title>
   <link rel="stylesheet" href="css/listquestion.css">
+  <link rel="stylesheet" href="assets/icofont/icofont.min.css">
 </head>
 
 <body>
@@ -13,6 +14,59 @@
     <div class="page">
 
       <div id="sentences" class="sentences">
+
+      <div class="sentence">
+          <div class="question">
+            <p id="question" class="question__text">Your path to learning English, step by step
+Cambridge English Qualifications are in-depth exams that make learning English enjoyable, effective and rewarding. Our unique approach encourages continuous progression with a clear path to improve language skills. We have qualifications for schools, general and higher education, and business.</p>
+          </div>
+
+          <div class="action">
+            <div class="action__view">
+              <div class="action__view--like">
+              <i class="icofont-ui-love"></i><span>12</span>
+              </div>
+              <div class="action__view--number">
+              <i class="fas fa-heart"></i>
+                33 lượt xem
+              </div>
+            </div>
+
+            <div class="action__btn">
+              <form action="#">
+                <button class="action__btn--item"><i class="icofont-love"></i> Yêu thích</button>
+                <button class="action__btn--item"><i class="icofont-speech-comments"></i>Trả lời</button>
+                <button class="action__btn--item"><i class="icofont-share"></i></i> Chia sẻ</button>
+              </form>
+            </div>
+          </div>
+
+          <div class="answer">
+            <div class="answer__container">
+              <img src="assets/images/register.jpg" alt="anhbacsi" class="answer__img">
+            </div>
+            <div class="answer_info">
+              <p class="answer__name">Bac si ka</p>
+              <p id="answer" class="answer__text">Together we inspire learners to go further
+Our range of free teaching resources, lesson plans and activities is designed to help you prepare your students for our exams and tests. We also have a range of teaching qualifications, courses and support to help you develop as a teacher.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="sentence">
+          <div class="question">
+            <p id="question" class="question__text">${element.content}</p>
+          </div>
+          <div class="answer">
+            <div class="answer__container">
+              <img src="assets/images/register.jpg" alt="anhbacsi" class="answer__img">
+            </div>
+            <div class="answer_info">
+              <p class="answer__name">Bac si ka</p>
+              <p id="answer" class="answer__text">${element.status}</p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="category">
@@ -29,9 +83,11 @@
 
     </div>
   </main>
-  <div id="test"></div>
 </body>
 <?php
 require "importjs.php"
 ?>
+<script>
+  getQuestion();
+</script>
 </html>
--- serviceAPI/question-service.js
+++ serviceAPI/question-service.js
@@ -1,63 +1,93 @@
 const question = {
-    idSpecialist : '',
-    content : '',
+  idSpecialist: '',
+  content: '',
 }
 
 const image = {
-    nameImage : ''
+  nameImage: ''
 }
 
 let headers = {
-	'Content-Type': 'application/json',
+  'Content-Type': 'application/json',
 }
-const addQuestion = async ()=> {
-    const req = {
-      
-        "idDepartments" : "xyzv",
-        "content" : "tao bi benh gi 3 ?",
-        "idUser" : "abc",
-        "view" : 1
-    }
-    const { data } = await api({
-        method: 'post',
-        url: "/question/addQuestion",
-        data: req,
-        headers
-    });
+const addQuestion = async (a) => {
+  const req = {
+    "content": `${a}`,
+    "id": "stri1231ng",
+    "idSpecialist": "stri123ng",
+    "idUser": "str11ing",
+    "status": "st123ring",
+    "view": 13
 
-    // const addQ = await axios.get(`user/login`, {
-    //     "email" : "trasceuroai@gmail.com",
-    //     "pass" : "123456"
-    // });
-    
-    console.log(data)
+  }
+
+  console.log(req);
+  const { data } = await api({
+    method: 'post',
+    url: "/question/add-question",
+    data: req,
+    headers
+  });
+
+  // const addQ = await axios.get(`user/login`, {
+  //     "email" : "trasceuroai@gmail.com",
+  //     "pass" : "123456"
+  // });
+
+  // console.log(data)
 }
 
 const getQuestion = async () => {
-    try {
-      console.log('calling');
-      const res = await axios.get(`${BASE_URL}`);
-  
-      const question = res.data;
-  
-      console.log(`GET: Here's the list of question`, question);
-  
-      process_data(question);
-  
-      return question;
-    } catch (e) {
-      console.error(e);
-    }
-  };
-  
-  function process_data(data) {
-    let sentences = document.getElementById('sentences');
-  
-    data.forEach(element => {
-      sentences.innerHTML += `
+  try {
+    const res = await axios.get('http://localhost:8080/question/list');
+    const question = res.data;
+    process_data(question);
+    return question;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// api select one question
+
+const getOneQuestion = async (id) => {
+  try {
+    const res = await axios.get(`http://localhost:8080/question/question?id=${id}`);
+    const question = res.data;
+    process_data(question);
+    return question;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// api update one question
+
+const updateQuestion = async (content, idUser, idSpecialist) => {
+  const req = {
+    "content": `${content}`,
+    "idSpecialist": `${idUser}`,
+    "idUser": `${idSpecialist}`,
+  }
+
+  const { data } = await api({
+    method: 'put',
+    url: "/question/updateQuestion",
+    data: req,
+    headers
+  });
+
+}
+
+
+function process_data(data) {
+  let sentences = document.getElementById('sentences');
+
+  data.forEach(element => {
+    sentences.innerHTML += `
         <div class="sentence">
           <div class="question">
-            <p id="question" class="question__text">${element.createdAt}</p>
+            <p id="question" class="question__text">${element.content}</p>
           </div>
           <div class="answer">
             <div class="answer__container">
@@ -65,11 +95,13 @@ const getQuestion = async () => {
             </div>
             <div class="answer_info">
               <p class="answer__name">Bac si ka</p>
-              <p id="answer" class="answer__text">${element.likeCount}</p>
+              <p id="answer" class="answer__text">${element.status}</p>
             </div>
           </div>
         </div>
       `;
-    });
-  }
-  
+  });
+}
+
+
+
