92778a63393ef42048a6f324417fa306906b860f
vuong <vuong.x510@gmail.com>
2021-01-31 20:44:20 +0700

    add post api comment

--- serviceAPI/question-service.js
+++ serviceAPI/question-service.js
@@ -79,26 +79,72 @@ const updateQuestion = async (content, idUser, idSpecialist) => {
 
 }
 
+// api add comment
+
+const addComment = async (a) => {
+  const req = {
+    "answer": `${a}`,
+  }
+  const { data } = await api({
+    method: 'post',
+    url: "user/comment",
+    data: req,
+    headers
+  });
+}
 
 function process_data(data) {
   let sentences = document.getElementById('sentences');
 
   data.forEach(element => {
     sentences.innerHTML += `
-        <div class="sentence">
-          <div class="question">
-            <p id="question" class="question__text">${element.content}</p>
-          </div>
-          <div class="answer">
-            <div class="answer__container">
-              <img src="assets/images/register.jpg" alt="anhbacsi" class="answer__img">
-            </div>
-            <div class="answer_info">
-              <p class="answer__name">Bac si ka</p>
-              <p id="answer" class="answer__text">${element.status}</p>
-            </div>
-          </div>
+    <div class="sentence">
+    <div class="question">
+      <p id="question" class="question__text">Your path to learning English, step by step
+        ${element.content}</p>
+    </div>
+
+    <div class="action">
+      <div class="action__view">
+        <div class="action__view--like">
+          <i class="icofont-ui-love"></i><span>12</span>
         </div>
+        <div class="action__view--number">
+          <i class="fas fa-heart"></i>
+          33 lượt xem
+        </div>
+      </div>
+
+      <div class="action__btn">
+
+        <button class="action__btn--item" id="btn-love"><i class="icofont-love"></i> Yêu thích</button>
+        <button class="action__btn--item btn-mid" onclick="answer()"><i class="icofont-speech-comments"></i>Trả lời</button>
+        <button class="action__btn--item"><i class="icofont-share"></i></i> Chia sẻ</button>
+
+      </div>
+    </div>
+
+    <div class="addanswer show" id="show">
+      <textarea name="" id="text-answer" cols="30" rows="10" placeholder="Nhập câu trả lời của bạn tại đây!"></textarea>
+      <button class="btn-send" onclick="addComment()">
+        <img src="assets/images/icon-send.png" alt="img">
+        <span class="">Gửi câu trả lời</span>
+      </button>
+    </div>
+
+    <!-- <div class="answer">
+      <div class="answer__container">
+        <img src="assets/images/register.jpg" alt="anhbacsi" class="answer__img">
+      </div>
+      <div class="answer_info">
+        <p class="answer__name">Bac si ka</p>
+        <p id="answer" class="answer__text">Together we inspire learners to go further
+          Our range of free teaching resources, lesson plans and activities is designed to help you prepare your
+          students for our exams and tests. We also have a range of teaching qualifications, courses and support
+          to help you develop as a teacher.</p>
+      </div>
+    </div> -->
+  </div>
       `;
   });
 }
