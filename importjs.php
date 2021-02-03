71e2dbe902075808ac64628dbf18728f8ab82b5d
tung <dangtung789.td@gmail.com>
2021-02-03 08:20:56 +0700

    update

--- importjs.php
+++ importjs.php
@@ -1,5 +1,6 @@
 <script src="./vendors/jquery/jquery-2.2.4.min.js"></script>
 <script src="./vendors/axios.min.js"></script>
+<script src="./vendors/moment.js"></script>
 <script src="./serviceAPI/config/config.js"></script>
 <script src="./serviceAPI/helper/helper.js"></script>
 <script src="./serviceAPI/user-service.js"></script>
