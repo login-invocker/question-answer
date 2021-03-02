4e8e7e79905a36a359f45530269d50e3f8e41812
tung <dangtung789.td@gmail.com>
2021-03-02 15:35:24 +0700

    update validate

--- serviceAPI/user-service.js
+++ serviceAPI/user-service.js
@@ -1,13 +1,19 @@
 const register = async () => {
-    const user = $("#email").val()
+    const email = $("#email").val()
     const pass = $("#pass").val()
     const username = $("#username").val()
 
+    const isPass = pass.length > 3 ? true: false
+    const isEmail  = validateEmail(email)
+
+    if(!isEmail) return $.notify("Email không đúng định dạng! Nhập lại email", "error");
+    if(!isPass) return $.notify("Mật khẩu quá ngắn, cần > 3 kí tự", "error");
+    
     if(pass == $("#re-pass").val()){
       
         const response = await axios.post('https://secret-plateau-56191.herokuapp.com/user/register', {
             "userPass": pass,
-            "userEmail": user,
+            "userEmail": email,
             "userName": username,
         })
 
