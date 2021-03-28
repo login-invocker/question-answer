0b08b740fc2fe778f15798cb60826e290c56ea08
tung <dangtung789.td@gmail.com>
2021-03-28 23:27:44 +0700

    update

--- detail-question/detail-question.js
+++ detail-question/detail-question.js
@@ -1,4 +1,5 @@
 const apiURLDoctorServer = `https://secret-plateau-56191.herokuapp.com`
+// const apiURLDoctorServer = `http://localhost:8080`
 
 let api = axios.create({
         baseURL: apiURLDoctorServer
